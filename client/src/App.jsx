c393c0e1b608445fecd200dd7f7e463288d4cc6d
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2024-01-30 01:43:53 +0530

    1111

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div> Yasitha </div>
+      <main className="app transition-all ease-in">
+        <Home />
+        <Canvas />
+        <Customizer />
+      </main>
         
     </>
   )
